1f22b1e38e97fa17a3b235456bd466cd89a8d56c
Barendra Biswal <barendrabiswal2000@gmail.com>
2024-08-01 00:27:24 +0530

    added col and paragraph

--- src/Screens/HomeScreen.js
+++ src/Screens/HomeScreen.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Carousel } from 'react-bootstrap';
+import { Carousel,Row,Col,Button } from 'react-bootstrap';
 const HomeScreen =()=>{
     return(
         <>
@@ -83,6 +83,49 @@ const HomeScreen =()=>{
                             />
                         </Carousel.Item>
             </Carousel>
+            <br/>
+            <div>
+                <Row>
+                    <Col>
+                    <h3><a href='#'>Vision</a></h3>
+                    <p className='p-style'>To plan the development of technical education, 
+                            to establish value-based and need-based education and training in engineering
+                         and technology,with a view to generate qualified and competent manpower,
+                         responsive to technological and societal needs.</p>
+                         <p>To plan the development of technical education, 
+                        to establish value-based and need-based education and training in engineering
+                         and technology, with a view to generate qualified and competent manpower,
+                         responsive to technological and societal needs.</p>
+                         <Button variant='warning'>Read More</Button>
+                    </Col>
+
+                    <Col>
+                    <h3><a href='#'>Mission</a></h3>
+                    <p className='p-style'>To plan the development of technical education, 
+                        to establish value-based and need-based education and training in engineering
+                         and technology, with a view to generate qualified and competent manpower,
+                         responsive to technological and societal needs.</p>
+                         <p>To plan the development of technical education, 
+                        to establish value-based and need-based education and training in engineering
+                         and technology, with a view to generate qualified and competent manpower,
+                         responsive to technological and societal needs.</p>
+                         <Button variant='warning'>Read More</Button>
+                    </Col>
+
+                    <Col>
+                    <h3><a href='#'>Mandate</a></h3>
+                    <p className='p-style'>To plan the development of technical education, 
+                        to establish value-based and need-based education and training in engineering
+                         and technology, with a view to generate qualified and competent manpower,
+                         responsive to technological and societal needs.</p>
+                         <p>To plan the development of technical education, 
+                        to establish value-based and need-based education and training in engineering
+                         and technology, with a view to generate qualified and competent manpower,
+                         responsive to technological and societal needs.</p>
+                         <Button variant='warning'>Read More</Button>
+                    </Col>
+                </Row>
+            </div>
         </div>
         </>
     )
